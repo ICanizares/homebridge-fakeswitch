ad5aa80ef75ad7821819b42d62e57b128233f832
ICanizares <icanizares@gmail.com>
2020-03-12 12:58:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@ICanizares 
-thncode
-/
-homebridge-fakeswitch
-0
-03
- Code Issues 1 Pull requests 1 Actions Projects 0 Wiki Security Insights
-homebridge-fakeswitch/index.js
-@thncode thncode init
-f2a3979 on 29 Jul 2018
-62 lines (48 sloc)  1.7 KB
-  
-Code navigation is available!
-Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
-
- Code navigation is available for this repository but data for this commit does not exist.
-
-Learn more or give us feedback
 "use strict";
 
 var Service, Characteristic, HomebridgeAPI;
@@ -33,7 +7,7 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-fakeswitch", "FakeSwitch", FakeSwitch);
+  homebridge.registerAccessory("homebridge-icr-fakeswitch", "FakeSwitch", FakeSwitch);
 }
 
 function FakeSwitch(log, config) {
@@ -86,15 +60,4 @@ FakeSwitch.prototype._setOn = function(on, callback) {
   
   callback();
 }
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
